f5e6f0d5018918b9dbe31c5ba7c2faa370dd7982
Nate Beaty <nate@clixel.com>
2014-10-06 20:33:40 -0500

    clean up code a bit

--- cronjobs.py
+++ cronjobs.py
@@ -17,8 +17,14 @@ class Twitter(Command):
     Weekly notices of oncoming deadlines
     """
 
+    def post_to_twitter(self,twitter_api,message):
+        try:
+            twitter_api.PostUpdate(message)
+            return ''
+        except:
+            return "There was an error posting '%s' to twitter. Please write Nate and mock him." % message
+
     def run(self):
-        # post link to twitter
         phrases = (
             'Heads up!',
             'Cartoonists!',
@@ -35,39 +41,28 @@ class Twitter(Command):
             'Keep calm:',
             'Mark your calendar!',
         )
-
         twitter_api = twitter.Api(consumer_key=config.TWITTER_CONSUMER_KEY, consumer_secret=config.TWITTER_CONSUMER_SECRET, access_token_key=config.TWITTER_ACCESS_TOKEN_KEY, access_token_secret=config.TWITTER_ACCESS_TOKEN_SECRET)
 
+        # cons that are a week away
         daily_notices = Condate.query.filter(Condate.start_date == date.today() + timedelta(days=7)).all()
         output = "Condates happening in a week: \n"
         for c in daily_notices:
             message = "%s %s is a week away! %s" % (random.choice(phrases), c.convention.title, c.convention.url)
-            try:
-                twitter_api.PostUpdate(message)
-            except:
-                output = "There was an error posting '%s' to twitter. Please write Nate and mock him." % msg
-            output = output + message
+            output = output + self.post_to_twitter(twitter_api,message) + message
 
+        # cons that are a month away
         monthly_notices = Condate.query.filter(Condate.start_date == date.today() + timedelta(days=30)).all()
         output = output + "\nCondates happening in a month: \n"
         for c in monthly_notices:
             message = "%s %s is a month away! %s" % (random.choice(phrases), c.convention.title, c.convention.url)
-            try:
-                twitter_api.PostUpdate(message)
-            except:
-                output = "There was an error posting '%s' to twitter. Please write Nate and mock him." % msg
-            output = output + message
+            output = output + self.post_to_twitter(twitter_api,message) + message
 
-        # find any cons that have registrations closing in a week
+        # cons that have registrations closing in a week
         weekly_registration_notices = Condate.query.filter(Condate.registration_closes == date.today() + timedelta(days=7)).all()
         output = output + "\nCondates with registration closing in a week: \n"
         for c in weekly_registration_notices:
             message = "%s %s registration closes in a week! %s" % (random.choice(phrases), c.convention.title, c.convention.url)
-            try:
-                twitter_api.PostUpdate(message)
-            except:
-                output = "There was an error posting '%s' to twitter. Please write Nate and mock him." % msg
-            output = output + message
+            output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
 
         print output
 
