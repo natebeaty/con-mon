04db246df03750fb895dc6ed60bf2fb3ec9e2c05
Nate Beaty <nate@clixel.com>
2014-10-10 14:39:06 -0500

    tone down autotweets

--- cronjobs.py
+++ cronjobs.py
@@ -46,21 +46,21 @@ class Twitter(Command):
         daily_notices = Condate.query.filter(Condate.start_date == date.today() + timedelta(days=7)).all()
         output = "Condates happening in a week: \n"
         for c in daily_notices:
-            message = "%s %s is a week away! %s" % (random.choice(phrases), c.convention.title, c.convention.url)
+            message = "%s %s is a week away. %s" % (random.choice(phrases), c.convention.title, c.convention.url)
             output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
 
         # cons that are a month away
         monthly_notices = Condate.query.filter(Condate.start_date == date.today() + timedelta(days=30)).all()
         output = output + "\nCondates happening in a month: \n"
         for c in monthly_notices:
-            message = "%s %s is a month away! %s" % (random.choice(phrases), c.convention.title, c.convention.url)
+            message = "%s %s is a month away. %s" % (random.choice(phrases), c.convention.title, c.convention.url)
             output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
 
         # cons that have registrations closing in a week
         weekly_registration_notices = Condate.query.filter(Condate.registration_closes == date.today() + timedelta(days=7)).all()
         output = output + "\nCondates with registration closing in a week: \n"
         for c in weekly_registration_notices:
-            message = "%s %s registration closes in a week! %s" % (random.choice(phrases), c.convention.title, c.convention.url)
+            message = "%s %s registration closes in a week. %s" % (random.choice(phrases), c.convention.title, c.convention.url)
             output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
 
         print output
