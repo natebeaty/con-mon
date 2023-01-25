c42b8f76ed39580a5bc807d7be2a27b54a7f8656
Nate Beaty <nate@clixel.com>
2023-01-25 11:40:19 -0500

    also post updates to mastodon from nightly cronjob

--- cronjobs.py
+++ cronjobs.py
@@ -6,35 +6,48 @@ from app import Condate,Tag,Phrase
 import config
 import twitter
 import random
+from mastodon import Mastodon
 
 app = Flask(__name__)
 app.config.from_object('config')
 manager = Manager(app)
 db = SQLAlchemy(app)
 
-class Twitter(Command):
+class Social(Command):
     """
-    Twitter alerts!
+    Twitter + Mastodon alerts!
     Weekly and monthly notices of approaching convention dates
     Weekly notices of approaching registration deadlines
     """
 
-    def post_to_twitter(self,twitter_api,message):
+    def post_to_twitter(self, twitter_api, message):
         try:
             twitter_api.PostUpdate(message)
             return ''
         except:
-            return "There was an error posting '%s' to twitter. Please write Nate and mock him." % message
+            return "There was an error posting to Twitter.\n"
+
+        return ''
+
+    def post_to_mastodon(self, mastodon_api, message):
+        try:
+            mastodon_api.toot(message)
+            return ''
+        except:
+            return "There was an error posting to Mastodon.\n"
 
     def run(self):
         twitter_api = twitter.Api(consumer_key=config.TWITTER_CONSUMER_KEY, consumer_secret=config.TWITTER_CONSUMER_SECRET, access_token_key=config.TWITTER_ACCESS_TOKEN_KEY, access_token_secret=config.TWITTER_ACCESS_TOKEN_SECRET)
+        mastodon_api = Mastodon(access_token=config.MASTODON_ACCESS_TOKEN, api_base_url=config.MASTODON_BASE_URL)
+
+        # get indie_tag for filtering
         indie_tag = Tag.query.filter(Tag.title == "Indie").first()
 
         # get least used phrases pool
         min_uses = db.session.query(db.func.min(Phrase.num_uses)).scalar()
         phrases = Phrase.query.filter(Phrase.num_uses == min_uses).all()
 
-        output = "-----------------\nTwitter cronjob: %s\n\n" % str(datetime.now())
+        output = "-----------------\nSocial cronjob: %s\n\n" % str(datetime.now())
 
         # cons that are a week away
         daily_notices = Condate.query.filter(Condate.start_date == date.today() + timedelta(days=7), Condate.published == True, Condate.cancelled == False).all()
@@ -46,9 +59,11 @@ class Twitter(Command):
                 phrase.num_uses += 1
                 phrases.remove(phrase)
                 message = "%s %s is a week away. %s" % (phrase, c.convention.title, c.convention.url)
+                output = output + self.post_to_mastodon(mastodon_api, message)
                 if c.convention.twitter:
                     message = message + " @%s" % (c.convention.twitter)
-                output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
+                output = output + self.post_to_twitter(twitter_api, message)
+                output = output + "\n" + message
 
         # cons that are a month away
         monthly_notices = Condate.query.filter(Condate.start_date == date.today() + timedelta(days=30), Condate.published == True, Condate.cancelled == False).all()
@@ -60,9 +75,11 @@ class Twitter(Command):
                 phrase.num_uses += 1
                 phrases.remove(phrase)
                 message = "%s %s is a month away. %s" % (phrase, c.convention.title, c.convention.url)
+                output = output + self.post_to_mastodon(mastodon_api, message)
                 if c.convention.twitter:
                     message = message + " @%s" % (c.convention.twitter)
-                output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
+                output = output + self.post_to_twitter(twitter_api, message)
+                output = output + "\n" + message
 
         # cons that have registrations closing in a week
         weekly_registration_notices = Condate.query.filter(Condate.registration_closes == date.today() + timedelta(days=7), Condate.published == True, Condate.cancelled == False).all()
@@ -74,15 +91,17 @@ class Twitter(Command):
                 phrase.num_uses += 1
                 phrases.remove(phrase)
                 message = "%s %s registration closes in a week. %s" % (phrase, c.convention.title, c.convention.url)
+                output = output + self.post_to_mastodon(mastodon_api, message)
                 if c.convention.twitter:
                     message = message + " @%s" % (c.convention.twitter)
-                output = output + self.post_to_twitter(twitter_api,message) + "\n" + message
+                output = output + self.post_to_twitter(twitter_api, message)
+                output = output + "\n" + message
 
         # commit updated phrase num_uses counts
         db.session.commit()
         print(output)
 
-manager.add_command('twitter', Twitter())
+manager.add_command('social', Social())
 
 if __name__ == "__main__":
     manager.run()
